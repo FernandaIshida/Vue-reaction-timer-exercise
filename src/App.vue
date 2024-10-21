2c848b258c330329197252a29ebadc40bcc0873b
Fernanda Ishida <fer.ishida@gmail.com>
2024-10-21 15:48:24 -0300

    Challenge-showing a results component

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,45 @@
 <template>
   <h1>Ninja Reaction Timer</h1>
   <button @click="start" :disabled="isPlaying">Play</button>
-  <!--Block v-if="isPlaying" :delay="delay"/> -->
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <!--Only show Results if the game is finished/showResults is true, gives acess to score, by binding it -->
+  <Results v-if="showResults" :score="score"/>
 </template>
 
 <script>
 import Block from './components/Block.vue'
+//Added downhere the import of "results"
+import Results from './components/Results.vue'
 
 export default {
   name: 'App',
-  components: { Block },
+  //Added "Results" in components
+  components: { Block, Results },
   data() {
     return {
       isPlaying: false,
       delay: null,
+      score: null,
+      showResults: false
     }
   },
   methods: {
     start() {
       this.delay = 2000 + Math.random() * 5000
       this.isPlaying = true
-    }
+      this.showResults = false
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false  
+      this.showResults = true    
+        }
   }
 }
 </script>
+<!--  Challenge
+      -when the game ends, show the results component
+      - output the score inside the results component -->
 
 <style>
 #app {
---
+++ src/components/Results.vue
@@ -0,0 +1,14 @@
+<template>
+    <p>Reaction time - {{ score }} ms</p>
+</template>
+
+<script>
+export default {
+    //accept the data
+    props: ['score']
+}
+</script>
+
+<style>
+
+</style>
